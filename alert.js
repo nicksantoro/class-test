623df6617f52bde7389c8233a323053d1827747c
Nicholas Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-08-03 14:39:32 -0700

    Updates made here

--- alert.js
+++ alert.js
@@ -1,1 +1,18 @@
-document.getElementById("testingThis").addEventListener("click", function() {alert("hello world"}));
+document.getElementById("testingThis").addEventListener("click", 
+function alertButton() {
+        alert("You clicked me");
+}
+alertButton());
+
+var x = document.getElementById("demo");   // Get the element with id="demo"
+x.style.color = "red";
+
+<script>
+  function clickAlert() {
+    alert("Alert!");
+}
+</script>
+
+<input type="button" onclick="clickAlert()" value="Click-2-Alert"
+
+     
